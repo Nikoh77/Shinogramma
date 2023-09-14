1ac93ca5fcc229721c84b148ea76aab5ddb105c8
Nikoh <nikoh@nikoh.it>
2023-09-14 12:57:57 +0200

    wip

--- shinogramma.py
+++ shinogramma.py
@@ -209,30 +209,39 @@ async def callback_handler(update: Update, context: CallbackContext):
             print(f'OK, server touched... \U0001F44D')
             await context.bot.send_message(chat_id=update.effective_chat.id, text=f'OK, done \U0001F44D')
             data=disAssebleMonitor(response.json())
-            if 'snap' in data.get('details'):
-                data['details']['snap']='1'
-                print(data)
-                response = requests.post(endpoint, str(data))
-                if response.status_code != 200:
-                    print(f'Error {response.status_code} something went wrong, request error \u26A0\ufe0f')
-                    print(response.text)
-                    await context.bot.send_message(chat_id=update.effective_chat.id, text='Error something went wrong, request error \u26A0\ufe0f')
-                    return
-                else:
-                    print(f'OK, done \U0001F44D')
-                    print(response.text)
-                    await context.bot.send_message(chat_id=update.effective_chat.id, text=f'OK, done \U0001F44D')
-            else:
-                pass
+            # if 'snap' in data.get('details'):
+            #     data['details']['snap']='1'
+            #     #print(data)
+            #     response = requests.post(endpoint, str(data))
+            #     if response.status_code != 200:
+            #         print(f'Error {response.status_code} something went wrong, request error \u26A0\ufe0f')
+            #         print(response.text)
+            #         await context.bot.send_message(chat_id=update.effective_chat.id, text='Error something went wrong, request error \u26A0\ufe0f')
+            #         return
+            #     else:
+            #         print(f'OK, done \U0001F44D')
+            #         print(response.text)
+            #         await context.bot.send_message(chat_id=update.effective_chat.id, text=f'OK, done \U0001F44D')
+            # else:
+            #     pass
 
 def disAssebleMonitor(response):
     print('disassembling monitor...')
     #response = response.json()
     monitor=response[0]
-    for key in monitor:
-        if type(monitor.get(key))==str:
-            if is_valid_json(monitor.get(key)):
-                monitor[key]=json.loads(monitor.get(key))
+    monitor['details']=json.loads(monitor.get('details'))
+    # monitor.pop('details')
+    keys=['mode', 'mid', 'name', 'tags', 'type', 'protocol', 'host', 'port', 'path', 'height', 'width', 'ext', 'fps', 'details']
+    test={}
+    for key in keys:
+        test[key]=monitor.get(key)
+    
+    
+    # for key in monitor:
+    #     if type(monitor.get(key))==str:
+    #         if is_valid_json(monitor.get(key)):
+    #             monitor[key]=json.loads(monitor.get(key))
+    print(test)
     return(monitor)
 
 def is_valid_json(my_json):
